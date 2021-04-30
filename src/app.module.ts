8464883ca5f62ea15498b4e97fd8c01143c4fd34
RyotArch <r.yamamoto.system@gmail.com>
2021-05-01 08:42:55 +0900

    [Update] update docker and connect  mysql with typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
+require('dotenv').config()
 
 @Module({
   imports: [
-   GraphQLModule.forRoot({
-     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-   }),
-   TodoModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: Number(process.env.MYSQL_PORT),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    TodoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
